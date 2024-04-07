2fbfd615d424b7e8edb654e0811a7f38956adbe1
LahiruHarshana <lharshana2002@gmail.com>
2024-04-08 01:10:21 +0530

    updated

--- src/user.models.ts
+++ src/user.models.ts
@@ -1,11 +1,14 @@
-import { Prop, Schema } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
 
+export type UserDocument = User & Document;
 @Schema()
 export class User {
     @Prop()
     username: string;
     @Prop()
     description: string;
-    @Prop()
+    @Prop({default: Date.now})
     date_added: Date;
 }
+export const UserSchema = SchemaFactory.createForClass(User);
