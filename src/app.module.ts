48e9bf436b505c871f2a03dd86579f981be9956c
LahiruHarshana <lharshana2002@gmail.com>
2024-04-09 12:30:32 +0530

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './user.models';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://lharshana2002:AcLQCZPDjxfSayEY@e-commerce.oc0xly1.mongodb.net/?retryWrites=true&w=majority&appName=e-commerce')],
+  imports: [MongooseModule.forRoot('mongodb+srv://lharshana2002:AcLQCZPDjxfSayEY@e-commerce.oc0xly1.mongodb.net/?retryWrites=true&w=majority&appName=e-commerce'),
+    MongooseModule.forFeature([{ name: 'user', schema: UserSchema }])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { User,UserDocument } from './user.models';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { privateDecrypt } from 'crypto';
+
+
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  constructor(
+    @InjectModel('user') private readonly userModel: Model<UserDocument>
+  ){}
+
+  //creating a user
+
 }
